879d778f9f751c0b9e8ab0cf66ac8b902f3799d8
Sasidhar Vanga <sasidhar@sasidhar.com>
2021-02-19 19:24:37 -0800

    Adding core feature

--- background.js
+++ background.js
@@ -1,14 +1,43 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+  const link = tab.url;
+
+
+  // var x = new XMLHttpRequest();
+  // x.open('GET', );
+  // x.onload = function() {
+  //     alert(x.responseText);
+  // };
+  // x.send();  
+
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   var activeTab = tabs[0];
+  //   console.log("Clicked on browser action");
+  //   chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  // });
 });
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
+
+    switch(request.msg) {
+      case "pull_data":
+        pullDataFromServer(request.data).then(function(data) {
+          console.log(data);
+          chrome.runtime.sendMessage({
+              msg: "got_response",
+              data
+          });    
+        });
+        break;
+      default:
+        console.log(`Message: ${request.msg}`);
+        console.log('default action in background');
+        break;
     }
+
+    // if( request.message === "open_new_tab" ) {
+    //   chrome.tabs.create({"url": request.url});
+    // }
   }
 );
+
---
+++ util.js
@@ -0,0 +1,16 @@
+function pullDataFromServer(url, data) {
+
+    return new Promise((resolve, reject) => {
+        const serverURL = "https://sasidhar.com"
+        const endPoint = `${serverURL}/data.json`;
+        const dataType = 'json';
+      
+        jQuery.get(endPoint, data, function(data) {
+            resolve(data);
+        }, dataType)
+        .fail(function() {
+            reject("Unable to complete AJAX Request.");
+        })
+    })
+
+}
